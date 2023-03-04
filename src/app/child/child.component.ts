48c39033eab39a66ee1b6548c61b64fad0f40eca
Afif Alfiano <m.afif@cakap.id>
2023-03-04 19:14:29 +0700

    compodoc

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,29 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+/**
+ * ChildComponent is component to show data from parent and emit data to parent.
+ */
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent {
+  /**Description: Variable to get value from parent component */
   @Input() name!: string;
+
+  /**Description:  Variable to assign value of two way data binding on input text */
   feedback = '';
+
+  /**Description: Variable to emit data from child to parent */
   @Output() childToParent: EventEmitter<string> = new EventEmitter<string>();
 
+  /**
+   * Description: onSendFeedback is function to emit data from child component into parent component}
+   * @param {string} feedback
+   * @returns {void}
+   * @memberof ChildComponent
+   */
   onSendFeedback(feedback: string) {
     this.childToParent.emit(feedback);
   }
