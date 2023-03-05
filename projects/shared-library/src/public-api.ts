ed166428f795dfabbb9d138bdf65c77fce7f9374
Afif Alfiano <m.afif@cakap.id>
2023-03-05 15:25:50 +0700

    fix

--- projects/shared-library/src/public-api.ts
+++ projects/shared-library/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/shared-library.service';
 export * from './lib/shared-library.component';
 export * from './lib/shared-library.module';
+export * from './lib/header/header.component';
+
